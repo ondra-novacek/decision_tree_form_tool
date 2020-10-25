215d76873d0f64d7961a447be5d27751558982b6
ondra-novacek <41262376+ondra-novacek@users.noreply.github.com>
2020-10-25 12:52:20 +0100

    migration questions and answers

--- database/migrations/2020_10_25_114730_create_answers_table.php
+++ database/migrations/2020_10_25_114730_create_answers_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateQuestionsTable extends Migration
+class CreateAnswersTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,10 @@ class CreateQuestionsTable extends Migration
      */
     public function up()
     {
-        Schema::create('questions', function (Blueprint $table) {
+        Schema::create('answers', function (Blueprint $table) {
             $table->increments('id');
             $table->string('text');
-            $table->boolean('isRoot');
-            $table->integer('answerDependency_id');
-            $table->integer('questionType_id');
+            $table->integer('question_id');
             $table->integer('user_id');
             $table->timestamps();
 
@@ -33,6 +31,6 @@ class CreateQuestionsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('questions');
+        Schema::dropIfExists('answers');
     }
 }
